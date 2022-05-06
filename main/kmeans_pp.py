36c75dbb459080ecf9db3faeae0c9813b8e66518
guyb1 <guyb181@gmail.com>
2022-05-06 12:01:30 +0300

    Getting k centroids

--- main/kmeans_pp.py
+++ main/kmeans_pp.py
@@ -1,22 +1,49 @@
+from operator import indexOf
 import sys
 import pandas as pd
 import numpy as np
-import kmeans_class_utils as class_utils
+from scipy.misc import central_diff_weights
+from kmeans_class_utils import Centroid, Env
 
 def main():
     try:
         args = load_args()
-        print(args)
-        input_data_frame = get_df(args.get(class_utils.Env.input_file1),
-                                  args.get(class_utils.Env.input_file2))
-        print(input_data_frame)
+        input_data_frame = get_df(args.get(Env.input_file1),
+                                  args.get(Env.input_file2))
+        np_array = pd.DataFrame.to_numpy(input_data_frame, dtype=float)
+        get_centriods(np_array, args.get(Env.k))
+        
+        
     except Exception as ex:
         print(ex)
         return
 
+
+def get_centriods(np_array, k):
+    np.random.seed(0)
+    n = np_array.shape[0]
+    c1 = np_array[np.random.choice(n)]
+    centroids = [c1]
+    weighted_p = [0]*100
+
+    
+    for _ in range(k - 1):
+        for j in range(n):
+            x = np_array[j]
+            closest_c = min(centroids, key=lambda c: np.linalg.norm(x - c))
+            weighted_p[j] = np.linalg.norm(x-closest_c)
+        distance_sum = sum(weighted_p)
+        weighted_p = list(map(lambda x: x / distance_sum, weighted_p))
+        new_cent_index = np.random.choice(n, p=weighted_p)
+        centroids.append(np_array[new_cent_index])
+    
+    print(centroids)
+
+
+
 def get_df(input_file1, input_file2):
-    df1 = pd.read_csv(input_file1, header=None)
-    df2 = pd.read_csv(input_file2, header=None)
+    df1 = pd.read_csv(input_file1, header=None, dtype=float)
+    df2 = pd.read_csv(input_file2, header=None, dtype=float)
     final_df = pd.merge(df1, df2, how='inner', on=0)
     return final_df.iloc[:, 1:]
 
@@ -30,24 +57,24 @@ def load_args():
     if len(inp) < 5 or len(inp) > 6:
         raise Exception("Invalid Input!")
     try:
-        args[class_utils.Env.k] = int(inp[1])
-        if args[class_utils.Env.k] < 1:
+        args[Env.k] = int(inp[1])
+        if args[Env.k] < 1:
             raise
 
-        args[class_utils.Env.input_file1] = inp[-2]
-        args[class_utils.Env.input_file2] = inp[-1]
+        args[Env.input_file1] = inp[-2]
+        args[Env.input_file2] = inp[-1]
         epsilon = float(inp[-3])
         if epsilon <= 0:
             raise
         else:
-            args[class_utils.Env.epsilon] = inp[-3]
+            args[Env.epsilon] = inp[-3]
 
         if len(inp) == 6: # Checking maxiter exists
             maxiter = int(inp[2])
             if maxiter <= 0: # Validating maxiter value
                 raise
             else:
-                args[class_utils.Env.maxiter] = maxiter # If exists, added to args.
+                args[Env.maxiter] = maxiter # If exists, added to args.
         
     except Exception:
         raise Exception("Invalid Input!")
